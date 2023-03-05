be364ef343da468fd1a5e0707730070e3467f02f
Abdulkerim Bilgi <115087916+Neccar43@users.noreply.github.com>
2023-03-05 17:34:10 +0300

    Added new weather icon

--- app/src/main/java/com/abdulkerim/weatherapp/service/WeatherService.kt
+++ app/src/main/java/com/abdulkerim/weatherapp/service/WeatherService.kt
@@ -2,10 +2,16 @@ package com.abdulkerim.weatherapp.service
 
 import com.abdulkerim.weatherapp.model.WeatherModel
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherService {
+    //Retrofit ile kullanım
     @GET("weather?")
     fun getWeather(@Query("q") cityName: String, @Query("appid") apiKey: String): Call<WeatherModel>
+
+    //coroutines ile kullanım
+    /*@GET("weather?")
+   suspend fun getWeather(@Query("q") cityName: String, @Query("appid") apiKey: String): Response<WeatherModel>*/
 }
--- app/src/main/java/com/abdulkerim/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/abdulkerim/weatherapp/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.abdulkerim.weatherapp.view
 
+import android.graphics.drawable.Icon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.abdulkerim.weatherapp.R
@@ -7,17 +8,24 @@ import com.abdulkerim.weatherapp.databinding.ActivityMainBinding
 import com.abdulkerim.weatherapp.model.WeatherModel
 import com.abdulkerim.weatherapp.service.WeatherService
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private val BASE_URL="https://api.openweathermap.org/data/2.5/"
+private val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 private lateinit var binding: ActivityMainBinding
+
+private var job: Job? = null
+private var exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+    println(throwable.localizedMessage)
+}
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
@@ -25,55 +33,128 @@ class MainActivity : AppCompatActivity() {
         //https://api.openweathermap.org/data/2.5/weather?q=Kutahya&appid=bf46a377f1ab9cc3a21ef0117596bf64
 
 
-
         binding.okButton.setOnClickListener {
-            if (binding.countryEdit.text.toString().isNotEmpty()){
-                val cityName= binding.countryEdit.text.toString()
+            if (binding.countryEdit.text.toString().isNotEmpty()) {
+                val cityName = binding.countryEdit.text.toString()
                 loadData(cityName)
-            }else{
-                Snackbar.make(it,"Lütfen şehir adı girin.",Snackbar.LENGTH_SHORT).show()
+            } else {
+                Snackbar.make(it, "Lütfen şehir adı girin.", Snackbar.LENGTH_SHORT).show()
             }
 
         }
 
 
-
     }
 
-    private fun loadData(cityName:String) {
-        val retrofit= Retrofit.Builder()
+    private fun loadData(cityName: String) {
+        val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())//Gson formatında gelen verileri dönüştürmemizi sağlıyor
             .build()
-        val service=retrofit.create(WeatherService::class.java)//retrofit ile apiyi birbirine bağladık
-        val call=service.getWeather(cityName,"bf46a377f1ab9cc3a21ef0117596bf64") //metodumuzu getirdik
+        //.create(WeatherService::class.java)//coroutines ile kullanırken bunu ekliyoruz
 
-        call.enqueue(object :Callback<WeatherModel>{
-            override fun onResponse(call: Call<WeatherModel>, response: Response<WeatherModel>) {
-                if (response.isSuccessful){
+        /*job = CoroutineScope(Dispatchers.IO+ exceptionHandler).launch {
+            val response = retrofit.getWeather(cityName, "bf46a377f1ab9cc3a21ef0117596bf64")
+            withContext(Dispatchers.Main){
+                if (response.isSuccessful) {
                     response.body()?.let {
-                        val temp=(it.main.temp)-273.15
-                        var condition:String?=null
-                        for (weather in it.weather){
-                           condition =weather.main
+                        val temp = (it.main.temp) - 273.15
+                        var condition: String? = null
+                        for (weather in it.weather) {
+                            condition = weather.main
 
                         }
-                        binding.tempetureText.text=temp.toString()
+                        binding.tempetureText.text = temp.toString()
 
-                        var havaDurumu=""
+
+                        var havaDurumu = ""
 
                         when (condition) {
-                            "Thunderstorm" -> havaDurumu="Fırtına"
-                            "Drizzle" -> havaDurumu="Çiselemeli Yağmur"
-                            "Rain" -> havaDurumu="Yağmurlu"
-                            "Snow" -> havaDurumu="Karlı"
-                            "Mist", "Smoke", "Haze" -> havaDurumu="Sisli"
-                            "Dust", "Sand", "Ash" -> havaDurumu="Tozlu"
-                            "Clear" ->havaDurumu= "Açık"
-                            "Clouds" -> havaDurumu="Bulutlu"
-                            else -> havaDurumu="Hava durumu belirsiz"
+                            "Thunderstorm" -> havaDurumu = "Fırtına"
+                            "Drizzle" -> havaDurumu = "Çiselemeli Yağmur"
+                            "Rain" -> havaDurumu = "Yağmurlu"
+                            "Snow" -> havaDurumu = "Karlı"
+                            "Mist", "Smoke", "Haze" -> havaDurumu = "Sisli"
+                            "Dust", "Sand", "Ash" -> havaDurumu = "Tozlu"
+                            "Clear" -> havaDurumu = "Açık"
+                            "Clouds" -> havaDurumu = "Bulutlu"
+                            else -> havaDurumu = "Hava durumu belirsiz"
                         }
-                        binding.coundationText.text=havaDurumu
+                        binding.coundationText.text = havaDurumu
+
+
+                    }
+                }
+            }
+
+        }*/
+
+        //Retrofit ile kullanım
+        val service =
+            retrofit.create(WeatherService::class.java)//retrofit ile apiyi birbirine bağladık
+        val call =
+            service.getWeather(cityName, "bf46a377f1ab9cc3a21ef0117596bf64") //metodumuzu getirdik
+
+        call.enqueue(object : Callback<WeatherModel> {
+            override fun onResponse(call: Call<WeatherModel>, response: Response<WeatherModel>) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        val temp = (it.main.temp) - 273.15
+                        var condition: String? = null
+                        for (weather in it.weather) {
+                            condition = weather.main
+
+                        }
+                        binding.tempetureText.text = temp.toString()
+
+                        var havaDurumu = ""
+                        val image = binding.coundationImage
+
+                        when (condition) {
+                            "Thunderstorm" -> {
+                                havaDurumu = "Fırtına"
+                                image.setImageResource(R.drawable.stormy)
+                            }
+                            "Drizzle" -> {
+                                havaDurumu = "Çiselemeli Yağmur"
+                                image.setImageResource(R.drawable.stormy)
+
+                            }
+                            "Rain" -> {
+                                havaDurumu = "Yağmurlu"
+                                image.setImageResource(R.drawable.rainy)
+
+                            }
+                            "Snow" -> {
+                                havaDurumu = "Karlı"
+                                image.setImageResource(R.drawable.snowy)
+
+                            }
+                            "Mist", "Smoke", "Haze" -> {
+                                havaDurumu = "Sisli"
+                                image.setImageResource(R.drawable.foggy)
+
+                            }
+                            "Dust", "Sand", "Ash" -> {
+                                havaDurumu = "Tozlu"
+                                image.setImageResource(R.drawable.dusty)
+
+                            }
+                            "Clear" -> {
+                                havaDurumu = "Açık"
+                                image.setImageResource(R.drawable.sunny)
+
+                            }
+                            "Clouds" -> {
+                                havaDurumu = "Bulutlu"
+                                image.setImageResource(R.drawable.cloudy)
+
+                            }
+                            else -> {
+                                havaDurumu = "Hava durumu belirsiz"
+                            }
+                        }
+                        binding.coundationText.text = havaDurumu
 
 
                     }
